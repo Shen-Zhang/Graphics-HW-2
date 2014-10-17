773274087e437a0ace21bb9cc6497b464c792abe
Shen-Zhang <zhangshe@grinnell.edu>
2014-10-17 21:03:39 +0200

    polyline works, selected mode works, but now it cannot remove a line which has only one control points

---
+++ CurveEditor/BezierNumber.h
@@ -0,0 +1,14 @@
+//
+//  BezierNumber.h
+//  practical2
+//
+//  Created by Shen on 10/17/14.
+//  Copyright (c) 2014 Shen Zhang. All rights reserved.
+//
+
+#ifndef practical2_BezierNumber_h
+#define practical2_BezierNumber_h
+
+
+
+#endif
